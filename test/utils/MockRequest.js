fa6e776186a9b9e707dfa5cf93e84613b07ff839
mruefenacht <ruefe@rcky.ch>
2014-05-14 16:59:59 +0200

    Fixed a typo

--- test/utils/MockRequest.js
+++ test/utils/MockRequest.js
@@ -1,7 +1,7 @@
 module.exports = function MockRequest(host, template, headers) {
-    this.host       = host;
-    this.template   = template;
-    this.headers = headers;
+    this.hostname       = host;
+    this.template       = template;
+    this.headers        = headers;
 
 
     this.getHeader = function(key, parse){
