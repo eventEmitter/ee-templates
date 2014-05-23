bf1842cb8b6fe822a04c15730204be593d1ca062
mruefenacht <ruefe@rcky.ch>
2014-05-23 20:02:09 +0200

    Added language parameter

--- test/MiddlewareTest.js
+++ test/MiddlewareTest.js
@@ -27,14 +27,21 @@ describe('Middleware', function(){
                 ]}
             );
 
+            var requestLanguage = new MockRequest('test.ch', 'test.language.nunjuck.html',
+                {'accept': [
+                    {key:'text', value: 'html'}
+                ]}
+            );
+
             var response = new MockResponse();
+            var responseLanguage = new MockResponse();
 
             middleware.request(request, response, function(){
                 it('should append a rendering method to the response', function(){
                     assert('render' in response);
                 });
 
-                response.render(200, {}, {ciao: 'Hallo'}, function(err){
+                response.render(200, 'en', {}, {ciao: 'Hallo'}, function(err){
                     it('which resolves the template and renders it', function(){
                         assert.equal(err, null);
                         assert.equal('Hallo: Test succeeded.', response.data);
@@ -43,6 +50,28 @@ describe('Middleware', function(){
                     it('should set the content type and the status correctly', function(){
                         assert.equal('text/html; charset=utf8', response.contentType);
                         assert.equal(200, response.status);
+                    });
+                });
+            });
+
+            middleware.request(requestLanguage, responseLanguage, function(){
+                it('should append a rendering method to the response', function(){
+                    assert('render' in responseLanguage);
+                });
+
+                responseLanguage.render(200, 'it', {}, {ciao: 'Hallo'}, function(err){
+                    it('which resolves the template and renders it with the language available', function(){
+                        assert.equal(err, null);
+                        assert.equal('Hallo: Test succeeded in it.', responseLanguage.data);
+                    });
+
+                    it('should set the content type and the status correctly', function(){
+                        assert.equal('text/html; charset=utf8', responseLanguage.contentType);
+                        assert.equal(200, responseLanguage.status);
+                    });
+
+                    it('should set the content language correctly', function(){
+                        assert.equal('it', responseLanguage.getHeader('content-language'));
                     });
                 });
             });
@@ -59,7 +88,7 @@ describe('Middleware', function(){
             var response1 = new MockResponse();
 
             middleware.request(request1, response1, function(){
-                response1.render(200, {}, {test: 'succeeded'}, function(err){
+                response1.render(200, 'en', {}, {test: 'succeeded'}, function(err){
                     it('if the request has an accept value of application/json it should render it as json, and ignore the template', function(){
                         assert.equal(err, null);
                         assert.equal('{"test":"succeeded"}', response1.data);
@@ -82,7 +111,7 @@ describe('Middleware', function(){
             var response1 = new MockResponse();
 
             middleware.request(request1, response1, function(){
-                response1.render(200, {}, {test: 'succeeded'}, function(err){
+                response1.render(200, 'en', {}, {test: 'succeeded'}, function(err){
                     it('if the request has another accept type it should fallback to json', function(){
                         assert(err==null);
                         assert.equal('{"test":"succeeded"}', response1.data);
@@ -106,7 +135,7 @@ describe('Middleware', function(){
             var response1 = new MockResponse();
 
             middleware.request(request1, response1, function(){
-                response1.render(200, {}, {test: 'succeeded'}, function(err){
+                response1.render(200, 'en', {}, {test: 'succeeded'}, function(err){
                     it('if there happens an error during the rendering it is passed to the callback', function(){
                         assert(!!err);
                     });
